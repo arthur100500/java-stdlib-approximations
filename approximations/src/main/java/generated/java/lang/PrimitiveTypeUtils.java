1693ca93669bfa0d8f76a83a06b28c0bd7d9fefa
MchKosticyn <mishakosticyn@yandex.ru>
2024-08-05 15:43:57 +0300

    [fix] fixed approximations

---
+++ approximations/src/main/java/generated/java/lang/PrimitiveTypeUtils.java
@@ -0,0 +1,8 @@
+package generated.java.lang;
+
+public class PrimitiveTypeUtils {
+
+    public static Class<?> getPrimitiveClass(java.lang.String ignoredName) {
+        return null;
+    }
+}
--- approximations/src/main/java/generated/org/springframework/boot/RequestParamMethodArgumentResolverImpl.java
+++ approximations/src/main/java/generated/org/springframework/boot/RequestParamMethodArgumentResolverImpl.java
@@ -6,36 +6,11 @@ import org.springframework.lang.Nullable;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.ModelAndViewContainer;
-import org.usvm.api.Engine;
+
+import static generated.org.springframework.boot.SymbolicValueFactory.createSymbolic;
 
 @Approximate(org.springframework.web.method.annotation.RequestParamMethodArgumentResolver.class)
 public class RequestParamMethodArgumentResolverImpl {
-
-    private static boolean argIsInjected(String typeName) {
-        return typeName.equals("org.springframework.ui.Model")
-                || typeName.equals("org.springframework.ui.ModelMap")
-                || typeName.contains("java.util.Map") // TODO: Map<String, Object>
-                || typeName.contains("org.springframework.validation.BindingResult");
-    }
-
-    private static Object createSymbolic(Class<?> type) {
-        if (type == boolean.class)
-            return Engine.makeSymbolicBoolean();
-        if (type == int.class)
-            return Engine.makeSymbolicInt();
-        if (type == long.class)
-            return Engine.makeSymbolicLong();
-        if (type == float.class)
-            return Engine.makeSymbolicFloat();
-        if (type == byte.class)
-            return Engine.makeSymbolicByte();
-        if (type == char.class)
-            return Engine.makeSymbolicChar();
-        if (type == short.class)
-            return Engine.makeSymbolicShort();
-
-        return Engine.makeSymbolic(type);
-    }
 
     public final Object resolveArgument(MethodParameter parameter, @Nullable ModelAndViewContainer mavContainer,
                                         NativeWebRequest webRequest, @Nullable WebDataBinderFactory binderFactory) throws Exception {

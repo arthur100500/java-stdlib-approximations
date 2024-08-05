b9fb01513e89ef615ee87a5bcc0543968e76f28a
MchKosticyn <mishakosticyn@yandex.ru>
2024-08-05 17:56:21 +0300

    updated jacodb version

--- approximations/build.gradle.kts
+++ approximations/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 private val jacodbPackage = "com.github.UnitTestBot.jacodb"
-private val jacodbVersion = "890624770b" // jacodb neo branch
+private val jacodbVersion = "00164e304b" // jacodb neo branch
 
 dependencies {
     compileOnly("$jacodbPackage:jacodb-api-jvm:$jacodbVersion")

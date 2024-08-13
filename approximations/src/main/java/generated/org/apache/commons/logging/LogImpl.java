4dbc6fb8d37361f9045147100635685f58f84af2
MchKosticyn <mishakosticyn@yandex.ru>
2024-08-13 16:18:39 +0300

    [fix] fixed 'Log' approximation

--- approximations/src/main/java/generated/org/apache/commons/logging/LogImpl.java
+++ approximations/src/main/java/generated/org/apache/commons/logging/LogImpl.java
@@ -4,53 +4,53 @@ import org.jacodb.approximation.annotation.Approximate;
 
 @SuppressWarnings("unused")
 @Approximate(org.apache.commons.logging.Log.class)
-public interface LogImpl {
+public class LogImpl {
 
-    default boolean isFatalEnabled() {
+    final boolean isFatalEnabled() {
         return false;
     }
 
-    default boolean isErrorEnabled() {
+    final boolean isErrorEnabled() {
         return false;
     }
 
-    default boolean isWarnEnabled() {
+    final boolean isWarnEnabled() {
         return false;
     }
 
-    default boolean isInfoEnabled() {
+    final boolean isInfoEnabled() {
         return false;
     }
 
-    default boolean isDebugEnabled() {
+    final boolean isDebugEnabled() {
         return false;
     }
 
-    default boolean isTraceEnabled() {
+    final boolean isTraceEnabled() {
         return false;
     }
 
-    default void fatal(Object message) { }
+    final void fatal(Object message) { }
 
-    default void fatal(Object message, Throwable t) { }
+    final void fatal(Object message, Throwable t) { }
 
-    default void error(Object message) { }
+    final void error(Object message) { }
 
-    default void error(Object message, Throwable t) { }
+    final void error(Object message, Throwable t) { }
 
-    default void warn(Object message) { }
+    final void warn(Object message) { }
 
-    default void warn(Object message, Throwable t) { }
+    final void warn(Object message, Throwable t) { }
 
-    default void info(Object message) { }
+    final void info(Object message) { }
 
-    default void info(Object message, Throwable t) { }
+    final void info(Object message, Throwable t) { }
 
-    default void debug(Object message) { }
+    final void debug(Object message) { }
 
-    default void debug(Object message, Throwable t) { }
+    final void debug(Object message, Throwable t) { }
 
-    default void trace(Object message) { }
+    final void trace(Object message) { }
 
-    default void trace(Object message, Throwable t) { }
+    final void trace(Object message, Throwable t) { }
 }

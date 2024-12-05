8a384eff1d9a34e968be92b365a95bc9c8c31076
Arthur Alekseev <arthur100500@gmail.com>
2024-12-05 20:03:41 +0300

    [wip] adding argument resolvers in progress
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

---
+++ approximations/src/main/java/generated/org/springframework/boot/resolvers/RequestParamMapMethodArgumentResolverImpl.java
@@ -0,0 +1,24 @@
+package generated.org.springframework.boot.resolvers;
+
+import org.jacodb.approximation.annotation.Approximate;
+import org.springframework.core.MethodParameter;
+import org.springframework.lang.Nullable;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import stub.java.util.map.RequestMap;
+import stub.java.util.map.RequestMultiValueMap;
+
+@Approximate(RequestParamMapMethodArgumentResolverImpl.class)
+public class RequestParamMapMethodArgumentResolverImpl {
+    public Object resolveArgument(MethodParameter parameter, @Nullable ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, @Nullable WebDataBinderFactory binderFactory) throws Exception {
+
+        if (MultiValueMap.class.isAssignableFrom(parameter.getParameterType())) {
+            return new RequestMultiValueMap("HEADER");
+        }
+
+        return new RequestMap("PARAM");
+    }
+}
